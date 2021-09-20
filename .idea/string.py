3e813a3b302bbad6c328cd688e5da659a20bccb1
ahsunkh <ahsun45@gmail.com>
2021-09-20 14:20:39 +0500

    ss

---
+++ .idea/string.py
@@ -0,0 +1,6 @@
+print("today is a good day")
+print("hello" + " world")
+greeting="Hello"
+name=input("enter your name here ")
+
+print(greeting+" "+name)
---
+++ helloworld.py
@@ -0,0 +1,12 @@
+# print("hello World !")
+# print(1+2)
+# print(7*4)
+# print()
+# print("The End!", "or is it end?", "watch it more", 3)
+
+i=1
+while i<6:
+    print(i)
+    if i==3:
+     break
+    i +=1
